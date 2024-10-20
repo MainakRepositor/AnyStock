ef8bc059c13a72eec7195cc80f43f6ae13de40f4
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2024-10-20 22:33:28 +0530

    Update ML_models.py

--- ML_models.py
+++ ML_models.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sktime.forecasting.compose import TransformedTargetForecaster
 from sktime.forecasting.model_selection import temporal_train_test_split
-from sktime.performance_metrics.forecasting import mean_absolute_percentage_error
+from sktime.performance_metrics.forecasting import smape_loss
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
@@ -19,7 +19,9 @@ def select_regressor(selection):
         'Support Vector Machines': LinearSVR(),
         'Extra Trees': ExtraTreesRegressor(),
     }
+
     return regressors[selection]
+
 
 def forecast(data, horizon, model):
     high = data['High'].reset_index(drop=True)
@@ -30,35 +32,42 @@ def forecast(data, horizon, model):
     regressor = select_regressor(model)
     index = pd.bdate_range(start=data.index[-1], periods=(horizon + 1))
 
-    # Using TransformedTargetForecaster as a replacement for ReducedRegressionForecaster
-    forecast_high = TransformedTargetForecaster(steps=[("regressor", regressor)])
-    forecast_high.fit(high)
+    forecast_high = ReducedRegressionForecaster(regressor=regressor, window_length=window_length,
+                                                strategy='recursive')
+    forecast_high.fit(high, fh=fh)
     fore_high = forecast_high.predict(fh).to_numpy()
+    # print(type(fore_high))
     fore_high = np.insert(fore_high, 0, data['High'][-1])
+    # print(fore_high)
     fore_high = pd.DataFrame(fore_high, index=index)
     fore_high.columns = ['Forecast_High']
 
-    forecast_low = TransformedTargetForecaster(steps=[("regressor", regressor)])
-    forecast_low.fit(low)
+    forecast_low = ReducedRegressionForecaster(regressor=regressor, window_length=window_length,
+                                               strategy='recursive')
+    forecast_low.fit(low, fh=fl)
     fore_low = forecast_low.predict(fl).to_numpy()
     fore_low = np.insert(fore_low, 0, data['Low'][-1])
     fore_low = pd.DataFrame(fore_low, index=index)
     fore_low.columns = ['Forecast_Low']
 
     data_final = pd.concat([data, fore_high, fore_low], axis=1)
+   # return data_final
 
-    # Performance Metrics
     y_train, y_test = temporal_train_test_split(high, test_size=horizon)
+    window_length = len(data['High'])-horizon-1
     fh = np.arange(y_test.shape[0]) + 1
-    forecaster = TransformedTargetForecaster(steps=[("regressor", regressor)])
-    forecaster.fit(y_train)
+    forecaster = ReducedRegressionForecaster(regressor=regressor, window_length=window_length,
+                                             strategy='recursive')
+    forecaster.fit(y_train, fh=fh)
     y_pred = forecaster.predict(fh)
-    smape_high = mean_absolute_percentage_error(y_pred, y_test, symmetric=True)
+    smape_high =  smape_loss(y_pred, y_test)
 
     y_train, y_test = temporal_train_test_split(low, test_size=horizon)
     fh = np.arange(y_test.shape[0]) + 1
-    forecaster.fit(y_train)
+    forecaster = ReducedRegressionForecaster(regressor=regressor, window_length=window_length,
+                                             strategy='recursive')
+    forecaster.fit(y_train, fh=fh)
     y_pred = forecaster.predict(fh)
-    smape_low = mean_absolute_percentage_error(y_pred, y_test, symmetric=True)
+    smape_low =  smape_loss(y_pred, y_test)
 
-    return [data_final, smape_high, smape_low]
+    return [data_final,smape_high ,smape_low]
